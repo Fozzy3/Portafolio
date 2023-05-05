145aa21fd74de93661323bede0d838456de772df
Hector Villa <hhvilla@gmail.com>
2023-05-05 17:37:06 -0500

    Try fix navbar errors, not yet

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,6 +8,39 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
   constructor(){}
 
-  title = 'Portafolio';
-  navbarFlag = false;
+  navbar = [
+    {
+      "icon": "bx bx-bar-chart-square",
+      "name": "Presentacion",
+      "routerLink": "/web/presentacion"
+    },
+    {
+      "icon": "bx bx-line-chart",
+      "name": "Workout Statistic",
+      "routerLink": "/web/projects"
+    },
+    {
+      "icon": "bx bx-calendar-event",
+      "name": "Workout Plan",
+      "routerLink":"/gaming/presentation"
+    },
+    {
+      "icon": "bx bxs-bolt",
+      "name": "Distance Map",
+      "routerLink":"/gaming/services"
+    },
+    {
+      "icon": "bx bxs-heart-circle",
+      "name": "Diet Food Menu",
+      "routerLink":"/applications/presentation"
+    },
+    {
+      "icon": "bx bxs-pie-chart-alt",
+      "name": "Personal Record",
+      "routerLink":"/applications/services"
+    }
+  ]
+
+  active:Boolean = false;
+
 }
